1e4cd64d63d69cc11a3aa4dacbf993e620ff591d
Artyom Drozdov <artyomcool2@gmail.com>
2016-06-12 23:03:45 +0300

    Failing test case: entity identity for String ids

--- processor/src/test/groovy/com/github/artyomcool/dante/AnnotationProcessorTest.groovy
+++ processor/src/test/groovy/com/github/artyomcool/dante/AnnotationProcessorTest.groovy
@@ -416,6 +416,95 @@ class AnnotationProcessorTest extends AbstractAptTest {
     }
 
     @Test
+    void integerIdentity() {
+        DaoRegistry registry = generateRegistry([[
+            fullClassName: "test.T",
+            sourceFile: """
+                package test;
+
+                import java.util.List;
+                import com.github.artyomcool.dante.annotation.*;
+
+                @Entity
+                public class T {
+
+                    @Id
+                    Long id;
+
+                    @Queries(T.class)
+                    public interface TestQuery {
+
+                        @Query(where = "id = \$id")
+                        T byId(long id);
+
+                    }
+
+                }
+            """
+        ]])
+        DaoMaster master = new DaoMaster({database}, registry)
+        master.init()
+
+        def testQueryClass = registry.class.classLoader.loadClass('test.T$TestQuery')
+        def queries = registry.queries(testQueryClass)
+
+        def dao = registry.dao[0]
+        def e = dao.createEntity()
+        e.id = 1
+        dao.insert(e)
+
+        def q1 = queries.byId(1)
+        def q2 = queries.byId(1)
+
+        assert q1.is(q2) && q1.is(e)
+    }
+
+    @Ignore('No implementation for cache with string keys')
+    @Test
+    void stringIdentity() {
+        DaoRegistry registry = generateRegistry([[
+            fullClassName: "test.T",
+            sourceFile: """
+                package test;
+
+                import java.util.List;
+                import com.github.artyomcool.dante.annotation.*;
+
+                @Entity
+                public class T {
+
+                    @Id(iWillSetIdByMySelf = true)
+                    String id;
+
+                    @Queries(T.class)
+                    public interface TestQuery {
+
+                        @Query(where = "id = \$id")
+                        T byId(String id);
+
+                    }
+
+                }
+            """
+        ]])
+        DaoMaster master = new DaoMaster({database}, registry)
+        master.init()
+
+        def testQueryClass = registry.class.classLoader.loadClass('test.T$TestQuery')
+        def queries = registry.queries(testQueryClass)
+
+        def dao = registry.dao[0]
+        def e = dao.createEntity()
+        e.id = 'str'
+        dao.insert(e)
+
+        def q1 = queries.byId('str')
+        def q2 = queries.byId('str')
+
+        assert q1.is(q2) && q1.is(e)
+    }
+
+    @Test
     void deleteById() {
         DaoRegistry registry = generateRegistry([[
             fullClassName: "test.T",
